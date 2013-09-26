4d18d713c35bea63d792b15fa571654247d5967b
Victor Moukhortov <victor.moukhortov@gmail.com>
2013-09-25 20:28:22 -0400

    Documenting configuration files, removing unused fields

--- config.py
+++ config.py
@@ -1,37 +1,55 @@
 #!/user/bin/env/python
 
+# Git origin URL
 git_url = ""
 
+# Staging server configuration
 staging = {
     'wordpressConfig' : {
+        # Wordpress databaes name
         "DB_NAME" : "",
+        # Wordpress database user
         "DB_USER" : "",
+        # Wordpress database user password
         "DB_PASSWORD" : "",
+        # Wordpress database host
         "DB_HOST" : "localhost",
-        "DB_CHARSET" : "utf8",
-        "DB_COLLATE" : "",
+        # URL to Wordpress installation - eg. http://myproject.com/blog/wp
         "WP_HOME" : "",
-        "WP_SITEURL" : ""
+        # URL to blog home page - eg. http://myproject.com/blog
+        "WP_SITEURL" : "",
     },
+    # Absolute path to install directory - eg. /var/www/html/myproject
     "directory" : "",
+    # SSH user and host - eg. me@myproject.com
     "host" : ""
 }
 
+# Production server configuration
 production = {
     'wordpressConfig' : {
+        # Wordpress databaes name
         "DB_NAME" : "",
+        # Wordpress database user
         "DB_USER" : "",
+        # Wordpress database user password
         "DB_PASSWORD" : "",
+        # Wordpress database host
         "DB_HOST" : "localhost",
-        "DB_CHARSET" : "utf8",
-        "DB_COLLATE" : "",
+        # URL to Wordpress installation - eg. http://myproject.com/blog/wp
         "WP_HOME" : "",
-        "WP_SITEURL" : ""
+        # URL to blog home page - eg. http://myproject.com/blog
+        "WP_SITEURL" : "",
     },
+    # Absolute path to install directory - eg. /var/www/html/myproject
     "directory" : "",
+    # SSH user and host - eg. me@myproject.com
     "host" : ""
 }
-    
+
+# Number of auto-backups to maintain
 backup_count = 3
+# Directory to store the backups in (relative to project directory)
 backup_directory = 'db-backup'
+# Filename to prepent to automatically-generated backups.
 backup_filename = 'auto-backup'
--- local_config.py
+++ local_config.py
@@ -1,14 +1,21 @@
+# Local configuration
 local = {
     'wordpressConfig' : {
+        # Wordpress databaes name
         "DB_NAME" : "",
+        # Wordpress database user
         "DB_USER" : "",
+        # Wordpress database user password
         "DB_PASSWORD" : "",
+        # Wordpress database host
         "DB_HOST" : "localhost",
-        "DB_CHARSET" : "",
-        "DB_COLLATE" : "",
+        # URL to Wordpress installation - eg. http://localhost/myproject/wp
         "WP_HOME" : "",
+        # URL to blog home page - eg. http://localhost/myproject
         "WP_SITEURL" : "",
     },
+    # Absolute path to install directory - eg. /var/www/html/myproject
     "directory" : "",
+    # SSH user and host - eg. me@localhost
     "host" : ""
 }
